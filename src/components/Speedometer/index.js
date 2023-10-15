b1b7c39f95ebb26f5c1d17bf77302baa612b4bc6
krishjp <krish4515.gk@gmail.com>
2023-10-15 06:45:43 +0000

    createa increase and decrease  speed

---
+++ src/components/Speedometer/index.js
@@ -0,0 +1,52 @@
+import {Component} from 'react'
+import './index.css'
+
+class Speedometer extends Component {
+  state = {count: 0}
+
+  onIncrease = () => {
+    const {count} = this.state
+
+    if (count < 200) {
+      this.setState(prevState => ({count: prevState.mph + 10}))
+    }
+  }
+
+  onDecrease = () => {
+    const {count} = this.state
+
+    if (count < 0) {
+      this.setState(prevState => ({count: prevState.mph - 10}))
+    }
+  }
+
+  render() {
+    const {count} = this.state
+    return (
+      <div className="speedometer-container">
+        <h1 className="main-heading">SPEEDOMETTER</h1>
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png "
+          alt="speedometer"
+        />
+        <h1 className="heading">Speed is {count}mph</h1>
+        <p className="description">Min Limit is 0mph Max Limit is 200mph</p>
+        <div>
+          <button className="buttons" type="button" onClick={this.onIncrease}>
+            Accelerate
+          </button>
+          <button
+            className="buttons break-button"
+            type="button"
+            onClick={this.onDecrease}
+          >
+            Apply Break
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Speedometer
